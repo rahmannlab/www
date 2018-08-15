b6137871c406ca9aa083f3b17cc71cbc1773e11c
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-08-15 13:37:11 +0200

    comment

--- .ci/publish.sh
+++ .ci/publish.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# decrypt ssh key
-openssl aes-256-cbc -K $encrypted_dba755bd9789_key -iv $encrypted_dba755bd9789_iv -in .ci/travis_key.enc -out .ci/travis_key -d
+# decrypt ssh key (the encrypted_... variables have been set in Travis)
+openssl aes-256-cbc -K $encrypted_dba755bd9789_key -iv $encrypted_dba755bd9789_iv \
+  -in .ci/travis_key.enc -out .ci/travis_key -d
+# the public part of the key has been added as deploy key in
+# https://github.com/rahmannlab/rahmannlab.github.io/settings/keys
