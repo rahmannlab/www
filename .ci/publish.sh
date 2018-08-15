a702d1df2d1c816e0d80f433a4719d9fa1d5a6e0
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-08-15 13:49:37 +0200

    next try to make travis push

--- .ci/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# decrypt ssh key (the encrypted_... variables have been set in Travis)
-openssl aes-256-cbc -K $encrypted_dba755bd9789_key -iv $encrypted_dba755bd9789_iv \
-  -in .ci/travis_key.enc -out .ci/travis_key -d
-# the public part of the key has been added as deploy key in
-# https://github.com/rahmannlab/rahmannlab.github.io/settings/keys
