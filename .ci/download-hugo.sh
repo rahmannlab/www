bde8c17846e10269bee03d969d4c6b5266fb6861
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-08-17 20:39:09 +0200

    make it a configurable variable, which version to choose

--- .ci/download-hugo.sh
+++ .ci/download-hugo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+version="0.46"
 dest=$(dirname "${BASH_SOURCE[0]}")
-url="https://github.com/gohugoio/hugo/releases/download/v0.46/hugo_0.46_Linux-64bit.tar.gz"
+url="https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_${version}_Linux-64bit.tar.gz"
 wget $url -O - | tar xzf - -C $dest hugo 
 
