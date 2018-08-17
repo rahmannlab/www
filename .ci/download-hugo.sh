c252fdd83b722a4c311dba86b1dd9e1272bd3a81
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2018-08-17 20:39:27 +0200

    travis: upgrade to hugo version 0.47

--- .ci/download-hugo.sh
+++ .ci/download-hugo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version="0.46"
+version="0.47"
 dest=$(dirname "${BASH_SOURCE[0]}")
 url="https://github.com/gohugoio/hugo/releases/download/v${version}/hugo_${version}_Linux-64bit.tar.gz"
 wget $url -O - | tar xzf - -C $dest hugo 
